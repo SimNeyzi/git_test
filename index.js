28f1eff3e834bece8eac07d4f8ae267a83f56927
Sim Neyzi <simneyzi@gmail.com>
2023-01-25 17:13:19 -0800

    add play func

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  return Math.floor(Math.random() * options.length);
+  const random = Math.floor(Math.random() * options.length);
+  return options[random];
 }
+
+let computerSelection = getComputerChoice();
+let playerSelection = "rock";
+
+function play(computerSelection, playerSelection) {
+  if (computerSelection === "rock") {
+    if (playerSelection === "paper") {
+      console.log("YOU win! Paper beats rock.");
+    } else if (playerSelection === "scissors") {
+      console.log("You lose! Rock beats scissors");
+    } else {
+      console.log("TIGHT! Another game?");
+    }
+  }
+
+  if (computerSelection === "paper") {
+    if (playerSelection === "scissors") {
+      console.log("YOU win! Scissors beats paper.");
+    } else if (playerSelection === "rock") {
+      console.log("You lose! Paper beats rock");
+    } else {
+      console.log("TIGHT! Another game?");
+    }
+  }
+
+  if (computerSelection === "scissors") {
+    if (playerSelection === "rock") {
+      console.log("YOU win! Rock beats scissors.");
+    } else if (playerSelection === "paper") {
+      console.log("You lose! Scissors beats paper");
+    } else {
+      console.log("TIGHT! Another game?");
+    }
+  }
+}
+
+console.log("hey");
+play(computerSelection, playerSelection);
