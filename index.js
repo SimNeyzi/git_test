eecc18c212e2265acc794cdb1db3706fb714ccc3
Sim Neyzi <simneyzi@gmail.com>
2023-01-27 14:50:17 -0800

    add score board

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ let win = "YOU win!",
   lose = "You lose!",
   tie = "Tie game!";
 
+let computerScore = 0,
+  playerScore = 0;
+
 function playRound(computerSelection, playerSelection) {
   playerSelection = playerSelection.toLowerCase();
   if (computerSelection === "rock") {
@@ -47,22 +50,31 @@ const userInput = document.querySelector(".user-input");
 userInput.addEventListener("click", (e) => {
   let computerSelection = getComputerChoice();
   let playerSelection = e.target.id;
-  console.log(playRound(computerSelection, playerSelection));
+  let result = playRound(computerSelection, playerSelection);
+
+  // add selection to score board
+  let playerSelectionImg = document.createElement("img");
+  playerSelectionImg.src = `./images/${playerSelection}.png`;
+  let playerScoreEl = document.querySelector(".player");
+  playerScoreEl.appendChild(playerSelectionImg);
+
+  if (computerScore < 5 || playerScore < 5) {
+    game(result);
+  }
 });
 
-function game() {
-  let computerScore = 0,
-    playerScore = 0;
-
-  while (computerScore + playerScore < 5) {
-    let result = playRound(computerSelection, playerSelection);
-
-    if (result.includes("YOU win!")) {
-      playerScore++;
-    } else if (result.includes("You lose!")) {
-      computerScore++;
-    }
-
-    console.log(`Player: ${playerScore} - Computer: ${computerScore} `);
+function game(result) {
+  if (result.includes("YOU win!")) {
+    playerScore++;
+  } else if (result.includes("You lose!")) {
+    computerScore++;
   }
+
+  console.log(`Player: ${playerScore} - Computer: ${computerScore} `);
+
+  let playerScoreEl = document.querySelector(".player");
+  playerScoreEl.textContent = `Player: ${playerScore}`;
+
+  let computerScoreEl = document.querySelector(".computer");
+  computerScoreEl.textContent = `Computer: ${computerScore}`;
 }
